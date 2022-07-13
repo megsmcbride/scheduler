ec80bedfd81dca0aa77bbea5b7bde6cac5d4f6f9
megan <meganambermcbride@gmail.com>
2022-07-13 15:42:28 -0400

    Add interveriwer listite component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,24 @@
 import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
 
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  
+  const InterviewerListItems = props.interviewers.map(interviewer => {
+    return <InterviewerListItem   
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={() => props.setInterviewer(interviewer.id)}
+    />
+  })
 
   return (
-    <ul>
-    
-    </ul>
+    <section className="interviewers">
+    <h4 className="interviewers__header text--light">Interviewer</h4>
+    <ul className="interviewers__list">{InterviewerListItems}</ul>
+  </section>
   );
 }
