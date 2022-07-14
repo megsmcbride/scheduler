7534d788c0c436dfa5e9e330ea48271d599e3ade
megan <meganambermcbride@gmail.com>
2022-07-14 18:57:34 -0400

    Add header component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./style.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 
 export default function Appointment(props) {
     return (
-      <article className="appointment"></article>
+      <article className="appointment">
+        <Header
+          time={props.time}>
+        </Header>
+        {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student}/> : <Empty />}
+      </article>
     );
   }
