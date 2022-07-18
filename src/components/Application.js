ed5edd40857b68ace48c77d0896668d6fa93b24d
megan <meganambermcbride@gmail.com>
2022-07-18 12:26:24 -0400

    Add create appointment function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,40 +3,72 @@ import axios from "axios";
 import Appointment from "./Appointment";
 import DayList from "./DayList";
 import "components/Application.scss";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
-  const setDay = (day) => setState(state => ({ ...state, day }));
+  const setDay = (day) => setState((state) => ({ ...state, day }));
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios.put(`/api/appointments/${id}`, { interview })
+      .then(() => {
+        setState({
+          ...state,
+          appointments
+        });
+    })
+  }
+
+  function cancelInterview(id, interview=null)
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
-  const dailyInterviewers = getInterviewersForDay(state, state.day)
-  const schedule = dailyAppointments.map(appointment => {
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
+  const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-    
+
     return (
       <Appointment
-      key={appointment.id}
-      {...appointment}
-      interview={interview}
-      interviewers={dailyInterviewers}
-      />);
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+        interviewers={dailyInterviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+    );
+  });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
-    
-    useEffect(() => {
-      Promise.all([
-        axios.get('/api/days'),
-        axios.get('api/appointments'),
-        axios.get('/api/interviewers')
-      ]).then(all => {
-        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      });
-    }, []);
-    
+  }, []);
 
   return (
     <main className="layout">
@@ -48,11 +80,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            value={state.day}
-            onChange={setDay}
-          />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -60,9 +88,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {schedule}
-      </section>
+      <section className="schedule">{schedule}</section>
     </main>
   );
 }
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,24 +4,34 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "components/hooks/useVisualMode";
 
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
-  const CREATE = "CREATE"
+  const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+
+    transition(SAVING);
+
+    props.bookInterview(props.id, interview).then(() => transition(SHOW));
+  }
 
   return (
     <article className="appointment">
-      <Header
-        time={props.time}>
-      </Header>
+      <Header time={props.time}></Header>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
@@ -30,13 +40,16 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && (
-        <Form 
-        student={props.student}
-        interviewer={props.interviewer}
-        interviewers={props.interviewers}
-        onCancel={back}
+        <Form
+          student={props.student}
+          interviewer={props.interviewer}
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
         />
       )}
+      {mode === SAVING && (<Status message={"Saving"} 
+      />)}
     </article>
   );
 }
