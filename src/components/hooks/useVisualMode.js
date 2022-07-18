6ea5e57e466e453206da8e80c8eda029b5d829c2
megan <meganambermcbride@gmail.com>
2022-07-17 21:40:14 -0400

    Add hooks

---
+++ src/components/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(mode, replace = false) {
+    if (replace) {
+      return setMode(mode)
+    }
+    setMode(mode);
+    setHistory([...history, mode]);
+  }
+
+  function back() {
+    if (history.length >= 2) {
+      const prevHistory = history.splice(0, history.length -1)
+      setHistory(prevHistory)
+      setMode(prevHistory[prevHistory.length -1])
+    }
+  }
+
+  return { mode, transition, back };
+}
