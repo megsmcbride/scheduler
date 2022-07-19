fb65abc30228c54e96740bc9ae0ed6a4de56496e
megan <meganambermcbride@gmail.com>
2022-07-19 17:25:37 -0400

    Fix format

--- src/components/hooks/useVisualMode.js
+++ src/components/hooks/useVisualMode.js
@@ -6,7 +6,7 @@ export default function useVisualMode(initial) {
 
   function transition(mode, replace = false) {
     if (replace) {
-      return setMode(mode)
+      return setMode(mode);
     }
     setMode(mode);
     setHistory([...history, mode]);
@@ -14,9 +14,9 @@ export default function useVisualMode(initial) {
 
   function back() {
     if (history.length >= 2) {
-      const prevHistory = history.splice(0, history.length -1)
-      setHistory(prevHistory)
-      setMode(prevHistory[prevHistory.length -1])
+      const prevHistory = history.splice(0, history.length - 1);
+      setHistory(prevHistory);
+      setMode(prevHistory[prevHistory.length - 1]);
     }
   }
 
