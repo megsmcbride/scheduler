9dfb0afcfb2c6a1c5ec050cb552bc45aea08dc1f
megan <meganambermcbride@gmail.com>
2022-07-19 17:20:08 -0400

    Add error handling

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
   
   const reset = () => {
     setStudent("")
@@ -15,8 +16,23 @@ export default function Form(props) {
   const cancel = () => {
     reset()
     props.onCancel()
+    setError("")
   }
 
+  const validate = () => {
+    if (!student) {
+      setError("Student name cannot be blank")
+      return;
+    }
+
+    if(interviewer ===  null) {
+      setError("Please select an interviewer")
+      return;
+    }
+    setError("")
+    props.onSave(student, interviewer)
+  }
+ 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,8 +44,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
 
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={props.interviewers}
@@ -41,7 +59,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={() => validate()}>Save</Button>
         </section>
       </section>
     </main>
